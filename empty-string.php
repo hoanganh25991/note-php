2e7f8e7d20bc8d8052959a4a28b94d7ae71f8885
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-05-25 10:08:10 +0700

    empty string

---
+++ empty-string.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * check empty string by `empty`
+ */
+if("" == ''){
+    echo "string is empty";
+}
+
+if(empty("")){
+	echo "string is empty";
+}
+
+if("hoanganh" == ''){
+    echo "string is not empty";
+}
+
