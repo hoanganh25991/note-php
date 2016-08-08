59119cd0a8b2b29b0bda10dceec69f5dcf49cc13
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-08 14:53:22 +0700

    table look-up

---
+++ table-look-up.php
@@ -0,0 +1,46 @@
+<?php
+/*
+≥ 90.0% A
+< 90.0% B
+< 75.0% C
+< 65.0% D
+< 50.0% F
+*/
+//1. if-else
+$pointRanges = [
+	[0.0, 50.0],
+	[50.0, 65.0],
+	[65.0, 75.0],
+	[75.0, 90.0],
+	[90.0, 100.0]
+];
+
+$pointTitles = [
+	"F",
+	"D",
+	"C",
+	"B",
+	"A"
+];
+
+$point = readline("your point: ");
+
+$title = "no title";
+
+foreach($pointRanges as $index => &$pointRange){
+	if($point >= $pointRange[0] && $point < $pointRange[1]){
+		$title = $pointTitles[$index];
+	}
+}
+
+echo $title;
+
+function readline($prompt = null){
+    if($prompt){
+        echo $prompt;
+    }
+    $fp = fopen("php://stdin","r");
+    $line = rtrim(fgets($fp, 1024));
+    return $line;
+}
+
