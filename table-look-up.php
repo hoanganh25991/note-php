79b67e4fb1aa2f7844cda3952a4a7e8108f8babb
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-17 13:01:46 +0700

    link, alias for junction & hardlink

--- table-look-up.php
+++ table-look-up.php
@@ -30,6 +30,7 @@ $title = "no title";
 foreach($pointRanges as $index => &$pointRange){
 	if($point >= $pointRange[0] && $point < $pointRange[1]){
 		$title = $pointTitles[$index];
+		break;
 	}
 }
 
@@ -43,4 +44,21 @@ function readline($prompt = null){
     $line = rtrim(fgets($fp, 1024));
     return $line;
 }
+
+//2. stair-table, loop loop util reach the maximum
+$pointRanges2 = [50.0, 65.0, 75.0, 90.0, 100.0];
+$pointTitles = [	"F",	"D",	"C",	"B",	"A"];
+$maxLength = count($pointTitles);
+
+$index = 0;
+
+while($pointRanges2[$index] < $point && $index < $maxLength){
+	//loop until
+	//$point reach its maximum
+	$index++;
+}
+
+$title = $pointTitles[$index];
+
+echo $title;
 
